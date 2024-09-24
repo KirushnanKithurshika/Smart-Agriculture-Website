b75a14e8b6c3b17a758a00a84030bcc1f27fb27b
kithurshika Kirushnan <kirushnankithurshika@gmail.com>
2024-09-24 15:35:06 +0530

    index & main

--- frontend/smart-agri/src/main.jsx
+++ frontend/smart-agri/src/main.jsx
@@ -1,9 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// Main.jsx (previously App.jsx)
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
-  
-)
+function Main() {
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default Main;
+
